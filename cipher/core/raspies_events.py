1d90c811dc663359fb138eb93da59dc01e885729
valorun <ronan@collier-famille.fr>
2019-06-04 12:22:31 +0200

    Update raspies_events.py

--- cipher/core/raspies_events.py
+++ cipher/core/raspies_events.py
@@ -25,10 +25,10 @@ def on_raspi_connect(client, userdata, msg):
     newRaspi = {}
     newRaspi['id'] = raspi_id
     #newRaspi['address'] = address
-    mqtt.subscribe("raspi/"+raspi_id+"/#")
+    mqtt.subscribe('raspi/' + raspi_id + '/#')
     raspies = [r for r in raspies if r['id'] != raspi_id] #delete already existing one with the same id
     raspies.append(newRaspi)
-    socketio.emit("get_raspies", raspies, namespace="/client", broadcast=True)
+    socketio.emit('get_raspies', raspies, namespace='/client', broadcast=True)
 
 @mqtt.on_topic('server/raspi_disconnect')
 def on_raspi_disconnect(client, userdata, msg):
@@ -40,20 +40,20 @@ def on_raspi_disconnect(client, userdata, msg):
     raspi_id = data['id']
     logging.info('Raspberry '+raspi_id+' disconnected.')
     raspies = [r for r in raspies if r['id'] != raspi_id]
-    mqtt.unsubscribe("raspi/"+raspi_id+"/#")
-    socketio.emit("get_raspies", raspies, namespace="/client", broadcast=True)
+    mqtt.unsubscribe('raspi/' + raspi_id + '/#')
+    socketio.emit('get_raspies', raspies, namespace='/client', broadcast=True)
 
 
 @socketio.on('shutdown', namespace='/client')
 def shutdown():
-    logging.info("Shutdown rasperries")
+    logging.info('Shutdown rasperries')
     mqtt.publish('raspi/shutdown', 'shutdown')
 
 @socketio.on('reboot', namespace='/client')
 def reboot():
-    logging.info("Reboot rasperries")
+    logging.info('Reboot rasperries')
     mqtt.publish('raspi/reboot', 'reboot')
 
 @socketio.on('get_raspies', namespace='/client')
 def get_raspies():
-    emit("get_raspies", raspies, namespace="/client", broadcast=True)
+    emit('get_raspies', raspies, namespace='/client', broadcast=True)
