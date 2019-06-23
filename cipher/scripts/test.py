902162b19b1ab965a304a9d3d155a0e21d96c7c8
valorun <ronan@collier-famille.fr>
2019-06-23 20:07:04 +0200

    script modif

--- cipher/scripts/test.py
+++ cipher/scripts/test.py
@@ -5,6 +5,9 @@ from cipher.core.actions import speech
 def main(**kwargs):
     if 'flags' not in kwargs or len(kwargs['flags']) < 1:
         return
+    if 'slots' not in kwargs or len(kwargs['slots']) < 1:
+        return
 
     speech(kwargs['flags'][0])
+    speech(kwargs['slots'][0])
     return kwargs
--- cipher/scripts/wikipedia.py
+++ cipher/scripts/wikipedia.py
@@ -4,8 +4,6 @@ import logging
 from cipher.core.actions import speech
 
 def main(**kwargs):
-    if 'flags' not in kwargs or len(kwargs['flags']) < 1:
-        return
     if 'slots' not in kwargs or len(kwargs['slots']) < 1:
         return
     response = requests.get("https://fr.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&explaintext=1&titles=" + kwargs['slots'][0]['rawValue'] + "&exintro&exlimit=1&exsentences=1")
