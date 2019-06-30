90923a192125375a9a5ba9052713a48b42c3598b
valorun <ronan@collier-famille.fr>
2019-06-30 23:43:24 +0200

    ...

--- cipher/scripts/calculette.py
+++ cipher/scripts/calculette.py
@@ -1,9 +1,9 @@
-from cipher.core.actions import speech
+#from cipher.core.actions import speech
 
 def main(**kwargs):
     if 'slots' not in kwargs or len(kwargs['slots']) < 1:
         return
-
+        
     result = 0
     last_operator = None
     for slot in kwargs['slots']:
@@ -22,4 +22,33 @@ def main(**kwargs):
         elif slot['entity'] == 'operator':
             last_operator = slot['value']['value']
 
-    speech(str(int(result)))
+    print(str(int(result)))
+
+args = {}
+s1 = {}
+s1['entity'] = 'snips/number'
+s1['value'] = {}
+s1['value']['value'] = 2.0
+
+s2 = {}
+s2['entity'] = 'operator'
+s2['value'] = {}
+s2['value']['value'] = '+'
+
+s3 = {}
+s3['entity'] = 'snips/number'
+s3['value'] = {}
+s3['value']['value'] = 3.0
+
+s4 = {}
+s4['entity'] = 'operator'
+s4['value'] = {}
+s4['value']['value'] = '*'
+
+s5 = {}
+s5['entity'] = 'snips/number'
+s5['value'] = {}
+s5['value']['value'] = 2.0
+
+args['slots'] = [s1, s2, s3, s4, s5]
+main(**args)
