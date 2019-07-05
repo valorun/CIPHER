5d4ae1de3bcc7523ad46ab16817867df977eadaa
valorun <ronan@collier-famille.fr>
2019-07-05 11:20:23 +0200

    debug modif

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from cipher import create_app, setup_logger, socketio, mqtt
 import os
 import logging
 
-certfile=os.path.join(os.path.dirname(__file__), 'cert.pem')
-keyfile=os.path.join(os.path.dirname(__file__), 'key.pem')
+certfile = os.path.join(os.path.dirname(__file__), 'cert.pem')
+keyfile = os.path.join(os.path.dirname(__file__), 'key.pem')
+
+DEBUG = True
 
 if __name__ == '__main__':
-    setup_logger(debug=True)
-    app = create_app(debug=False)
+    setup_logger(debug=DEBUG)
+    app = create_app(debug=DEBUG)
     logging.info("Application started")
-    #socketio.run(app, host = '0.0.0.0', port = 5000, certfile=certfile, keyfile=keyfile)
-    socketio.run(app, host = '0.0.0.0', port = 5000)
+    if DEBUG:
+        socketio.run(app, host = '0.0.0.0', port = 5000)
+    else:
+        socketio.run(app, host = '0.0.0.0', port = 5000, certfile=certfile, keyfile=keyfile)
