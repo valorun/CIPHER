a7d9fe24725432fd0f9bd781edd640c205133ac6
valorun <ronan@collier-famille.fr>
2019-07-14 20:28:00 +0200

    add forgotten file

---
+++ cipher/plugins/speech/model.py
@@ -0,0 +1,11 @@
+from cipher.model import db
+from . import speech
+
+class Intent(db.Model):
+	intent = db.Column(db.String(50), primary_key=True)
+	script_name = db.Column(db.String(50))
+	sequence_id = db.Column(db.String(50), db.ForeignKey('sequence.id'))
+	enabled = db.Column(db.Boolean, nullable=False)
+
+	def __repr__(self):
+		return '<Intent %r>' % self.intent
