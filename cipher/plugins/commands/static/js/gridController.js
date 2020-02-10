3d43db61a62c57df15ba79b4e4bfa2e14575a53f
valorun <ronan@collier-famille.fr>
2020-02-10 20:30:09 +0100

    bla

--- cipher/plugins/commands/static/js/gridController.js
+++ cipher/plugins/commands/static/js/gridController.js
@@ -5,131 +5,140 @@
 
 /* exported gridController */
 const gridController = (() => {
-	'use strict';
-	
-	const DOM = {};
+    'use strict';
 
-	let grid = null;
-	const gridItems = [];
-	let buttonsDisabled = false;
+    const DOM = {};
+
+    let grid = null;
+    const gridItems = [];
+    let buttonsDisabled = false;
 
 
-	/* PUBLIC METHODS */
-	function init() {
-		cacheDom();
-		bindUIEvents();
-	}
+    /* PUBLIC METHODS */
+    function init() {
+        cacheDom();
+        bindUIEvents();
+    }
 
-	function addButton(buttonData, index = 0) {
-		if (actionAlreadyUsed(buttonData.action))
-			throw new AlreadyUsedError('Action is already used');
-		const newButton = CommandButton.fromJSON(buttonData);
+    function addButton(buttonData, index = 0) {
+        if (actionAlreadyUsed(buttonData.action))
+            throw new AlreadyUsedError('Action is already used');
+        const newButton = CommandButton.fromJSON(buttonData);
 
-		// containers elements needed for animation and position
-		const $outerWrapper = document.createElement('div');
-		$outerWrapper.classList.add('grid-item');
-		const $innerWrapper = document.createElement('div');
-		$innerWrapper.classList.add('grid-item-content');
-		$innerWrapper.appendChild(newButton.$el);
-		$outerWrapper.appendChild($innerWrapper);
+        // containers elements needed for animation and position
+        const $outerWrapper = document.createElement('div');
+        $outerWrapper.classList.add('grid-item');
+        const $innerWrapper = document.createElement('div');
+        $innerWrapper.classList.add('grid-item-content');
+        $innerWrapper.appendChild(newButton.$el);
+        $outerWrapper.appendChild($innerWrapper);
 
-		const newItem = grid.add($outerWrapper, {index: index});
-		newItem.commandButton = newButton; // link grid item to our custom button object
+        const newItem = grid.add($outerWrapper, {
+            index: index
+        });
+        newItem.commandButton = newButton; // link grid item to our custom button object
 
-		const newGridItem = new GridItem(newButton, newItem[0]);
-		newGridItem.onClose((gridItem) => {
-			const buttonIndex = gridItems.findIndex(gi => JSON.stringify(gi.commandButton) === JSON.stringify(gridItem.commandButton) );
-			gridItems.splice(buttonIndex, 1);
-			grid.remove(gridItem.item, {removeElements: true});
-		});
+        const newGridItem = new GridItem(newButton, newItem[0]);
+        newGridItem.onClose((gridItem) => {
+            const buttonIndex = gridItems.findIndex(gi => JSON.stringify(gi.commandButton) === JSON.stringify(gridItem.commandButton));
+            gridItems.splice(buttonIndex, 1);
+            grid.remove(gridItem.item, {
+                removeElements: true
+            });
+        });
 
-		gridItems.push(newGridItem);
-		
-		return newButton;
-	}
+        gridItems.push(newGridItem);
 
-	function disableButtons() {
-		gridItems.forEach(gi => {
-			gi.commandButton.disable();
-		});
-		buttonsDisabled = false;
-	}
+        return newButton;
+    }
 
-	function enableButtons() {
-		gridItems.forEach(gi => {
-			gi.commandButton.enable();
-		});
-		buttonsDisabled = true;
-	}
+    function disableButtons() {
+        gridItems.forEach(gi => {
+            gi.commandButton.disable();
+        });
+        buttonsDisabled = false;
+    }
 
-	function updateRelaysButtons(relaysStates) {
-		// get the relays and update the associated state
-		const relaysButtons = gridItems.map(gi => gi.commandButton).filter(b => b instanceof RelayButton);
-		relaysStates.map(r => {
-			r.relay = relaysButtons.find(b => b.action.relay === r.relay);
-			return r;
-		}).filter(r => r.relay !== undefined)
-			.forEach(r => r.state === 1 ? r.relay.activate() : r.relay.deactivate());
-	}
+    function enableButtons() {
+        gridItems.forEach(gi => {
+            gi.commandButton.enable();
+        });
+        buttonsDisabled = true;
+    }
 
-	function clear() {
-		grid.remove(grid.getItems(), {removeElements: true});
-	}
+    function updateRelaysButtons(relaysStates) {
+        // get the relays and update the associated state
+        const relaysButtons = gridItems.map(gi => gi.commandButton).filter(b => b instanceof RelayButton);
+        relaysStates.map(r => {
+            r.relay = relaysButtons.find(b => b.action.relay === r.relay);
+            return r;
+        }).filter(r => r.relay !== undefined)
+            .forEach(r => r.state === 1 ? r.relay.activate() : r.relay.deactivate());
+    }
 
-	function toJSON() {
-		return gridItems
-			.map(gi => {
-				const json = gi.commandButton.toJSON();
-				// add position information
-				json.index = grid.getItems().findIndex(item => item.getElement().innerHTML === gi.item.getElement().innerHTML);
-				return json;
-			});
-	}
+    function clear() {
+        grid.remove(grid.getItems(), {
+            removeElements: true
+        });
+    }
 
-	/* PRIVATE METHODS */
-	function bindUIEvents() {
-		grid = new Muuri('.grid', {dragEnabled: true,
-			dragStartPredicate: () => {return !buttonsDisabled;}
-		});
-	}
+    function toJSON() {
+        return gridItems
+            .map(gi => {
+                const json = gi.commandButton.toJSON();
+                // add position information
+                json.index = grid.getItems().findIndex(item => item.getElement().innerHTML === gi.item.getElement().innerHTML);
+                return json;
+            });
+    }
 
-	function cacheDom() {
-		DOM.$grid = document.getElementById('grid');
-	}
+    /* PRIVATE METHODS */
+    function bindUIEvents() {
+        grid = new Muuri('.grid', {
+            dragEnabled: true,
+            dragStartPredicate: () => {
+                return !buttonsDisabled;
+            }
+        });
+    }
 
-	/**
-	 * Check if a button with the same action already exists
-	 */
-	function actionAlreadyUsed(action){
-		let found = false;
-		gridItems.map(gi => gi.commandButton).forEach(b => {
-			if(JSON.stringify(b.action) === JSON.stringify(action)){
-				found = true;
-				return false;
-			}
-		});
-		return found;
-	}
+    function cacheDom() {
+        DOM.$grid = document.getElementById('grid');
+    }
 
-	return {
-		init: init,
-		addButton: addButton,
-		disableButtons: disableButtons,
-		enableButtons: enableButtons,
-		updateRelaysButtons: updateRelaysButtons,
-		clear: clear,
-		toJSON: toJSON
-	};
+    /**
+     * Check if a button with the same action already exists
+     */
+    function actionAlreadyUsed(action) {
+        let found = false;
+        gridItems.map(gi => gi.commandButton).forEach(b => {
+            if (JSON.stringify(b.action) === JSON.stringify(action)) {
+                found = true;
+                return false;
+            }
+        });
+        return found;
+    }
+
+    return {
+        init: init,
+        addButton: addButton,
+        disableButtons: disableButtons,
+        enableButtons: enableButtons,
+        updateRelaysButtons: updateRelaysButtons,
+        clear: clear,
+        toJSON: toJSON
+    };
 })();
 
 /* exported AlreadyUsedError */
 class AlreadyUsedError extends Error {
-	constructor(...params) {
-		super(...params);
-	
-		if(Error.captureStackTrace) {
-			Error.captureStackTrace(this, AlreadyUsedError);
-		}
-		this.name = 'AlreadyUsedError';
-	}
+    constructor(...params) {
+        super(...params);
+
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, AlreadyUsedError);
+        }
+        this.name = 'AlreadyUsedError';
+    }
 }
--- cipher/static/js/connection.js
+++ cipher/static/js/connection.js
@@ -6,64 +6,64 @@ const socket = io.connect(window.location.host + '/client');
 
 /* exported connectionManager */
 const connectionManager = (() => {
-	'use strict';
+    'use strict';
 
-	let voices = null;
-	let audio = null;
+    let voices = null;
+    let audio = null;
 
-	/* PUBLIC METHODS */
-	function init(){
-		voices = window.speechSynthesis.getVoices();
+    /* PUBLIC METHODS */
+    function init(){
+        voices = window.speechSynthesis.getVoices();
 
-		window.speechSynthesis.onvoiceschanged = () => {
-			voices = window.speechSynthesis.getVoices();
-		};
-		bindSocketIOEvents();
-	}
+        window.speechSynthesis.onvoiceschanged = () => {
+            voices = window.speechSynthesis.getVoices();
+        };
+        bindSocketIOEvents();
+    }
 
-	function speak(msg) {
-		if ('speechSynthesis' in window && voices !== null) {
-			const to_speak = new SpeechSynthesisUtterance(msg);
-			voices.forEach((e) =>{
-				if(Cookies.get('voice') === e.name){
-					to_speak.voice = e;
-				}
-			});
-			window.speechSynthesis.speak(to_speak);
-		}
-	}
+    function speak(msg) {
+        if ('speechSynthesis' in window && voices !== null) {
+            const to_speak = new SpeechSynthesisUtterance(msg);
+            voices.forEach((e) =>{
+                if(Cookies.get('voice') === e.name){
+                    to_speak.voice = e;
+                }
+            });
+            window.speechSynthesis.speak(to_speak);
+        }
+    }
 
-	/* PRIVATE METHODS */
-	function bindSocketIOEvents(){
-		socket.on('command', (msg) => {
-			console.log('Message from server: ', msg);
-		});
+    /* PRIVATE METHODS */
+    function bindSocketIOEvents(){
+        socket.on('command', (msg) => {
+            console.log('Message from server: ', msg);
+        });
 
-		//server response when a sentence must be play on the client
-		socket.on('response', (msg) => {
-			console.log('Message from server: ', msg);
-			speak(msg);
-		});
-		socket.on('play_sound', (sound_name) => {
-			if(audio != null ){
-				audio.pause();
-				audio = null;
-			}
-			else {
-				audio = new Audio(window.location.origin + '/play_sound/' + sound_name);
-				audio.play();
-			}
-		});
-		socket.on('connect', () => {
-			document.getElementById('socketErrorModal').style.display = 'none';
-		});
-		socket.on('disconnect', () => {
-			document.getElementById('socketErrorModal').style.display = 'block';
-		});
-	}
+        //server response when a sentence must be play on the client
+        socket.on('response', (msg) => {
+            console.log('Message from server: ', msg);
+            speak(msg);
+        });
+        socket.on('play_sound', (sound_name) => {
+            if(audio != null ){
+                audio.pause();
+                audio = null;
+            }
+            else {
+                audio = new Audio(window.location.origin + '/play_sound/' + sound_name);
+                audio.play();
+            }
+        });
+        socket.on('connect', () => {
+            document.getElementById('socketErrorModal').style.display = 'none';
+        });
+        socket.on('disconnect', () => {
+            document.getElementById('socketErrorModal').style.display = 'block';
+        });
+    }
 
-	return {
-		init: init,
-		speak: speak
-	};
+    return {
+        init: init,
+        speak: speak
+    };
 })();
