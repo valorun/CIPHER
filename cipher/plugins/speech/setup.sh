4120aa1db81973d860c8357f5c0b71da9192b5d1
valorun <ronan@collier-famille.fr>
2020-09-25 12:20:02 +0200

    Redone install script

--- cipher/plugins/speech/setup.sh
+++ cipher/plugins/speech/setup.sh
@@ -1,49 +1,23 @@
-#!/bin/bash
-
-install_program(){
-    while true; do
-        read -p "Do you wish to install this program ? " yn
-        case $yn in
-            [Yy]* ) sudo apt-get install "$1"; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-}
+#!/usr/bin/env bash
 
 ### requirements ###
-echo "Installing wget ..."
 install_program "wget"
-echo "Installing unzip ..."
 install_program "unzip"
 
+PLUGIN_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+echo "Plugin path: $PLUGIN_PATH"
+
+cd $PLUGIN_PATH
 wget https://github.com/marytts/marytts/releases/download/v5.2/marytts-5.2.zip -O marytts.zip
 unzip ./marytts.zip
+rm marytts.zip
 mv marytts-5.2 marytts
 cd marytts
 
-APP_PATH=$(cd $(dirname "$0") && pwd)
+PLUGIN_PATH=$(cd $(dirname "$0") && pwd)
 
-echo "Application path: $APP_PATH"
 echo "Installing marytts ..."
-$APP_PATH/bin/marytts-component-installer
+$PLUGIN_PATH/bin/marytts-component-installer
 
 ### add to startup ###
-if [ -e /etc/rc.local ]
-then
-    if grep -q "nohup sudo $APP_PATH/bin/marytts-server &" /etc/rc.local
-    then
-        echo "Program already added on startup."
-    else
-        while true; do
-            read -p "Do you wish to add this program on startup ? " yn
-            case $yn in
-                [Yy]* ) sed -i -e "\$i \\nohup sudo $APP_PATH/bin/marytts-server &\\n" /etc/rc.local; break;;
-                [Nn]* ) exit;;
-                * ) echo "Please answer yes or no.";;
-            esac
-        done
-    fi
-else
-    echo "No rc.local file found, can't add program on startup."
-fi
+add_to_startup "$PLUGIN_PATH/bin/marytts-server"
--- setup.sh
+++ setup.sh
@@ -1,67 +1,78 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 install_program(){
-    while true; do
-        read -p "Do you wish to install this program ? " yn
-        case $yn in
-            [Yy]* ) sudo apt-get install "$1"; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
+    if dpkg -s "$1" &>/dev/null; then
+        echo "$1 found"
+    else
+        echo "\"$1\" not found"
+        while true; do
+            dialog --title "C.I.P.H.E.R" --clear --yesno "Do you wish to install \"$1\" ?" 5 50
+            case $? in
+                0) echo "Installing package \"$1\" ..."; sudo apt-get install "$1"; break;;
+                1) clear; exit;;
+                *) echo "Please answer yes or no.";;
+            esac
+        done
+    fi
 }
 
+add_to_startup(){
+    echo "Adding $1 to startup ..."
+    if [ -e /etc/rc.local ]
+    then
+        if grep -q "nohup sudo $1 &" /etc/rc.local
+        then
+            echo "Program already added on startup."
+        else
+            while true; do
+                dialog --title "C.I.P.H.E.R" --clear --yesno "Do you wish to add this program on startup ?" 5 50
+                case $? in
+                    0) sed -i -e "\$i \\nohup sudo $1 &\\n" /etc/rc.local; break;;
+                    1) exit;;
+                    * ) echo "Please answer yes or no.";;
+                esac
+            done
+        fi
+    else
+        echo "No rc.local file found, can't add program on startup."
+    fi
+}
+
+if type "dialog" &>/dev/null; then
+    echo "Dialog found"
+else
+    echo "Dialog not found"
+    sudo apt-get install dialog
+fi
+
 ### requirements ###
-if type "python3" &>/dev/null; then
-    echo "Python found"
-else
-    echo "Python3 not found"
-    install_program "python3"
-fi
-
-if type "pip3" &>/dev/null; then
-    echo "Pip found"
-else
-    echo "Pip not found"
-    install_program "python3-pip"
-fi
-
-echo "Installing mplayer ..."
+install_program "python3"
+install_program "python3-pip"
 install_program "mplayer"
-echo "Installing mosquitto ..."
 install_program "mosquitto"
-echo "Installing mosquitto-clients ..."
 install_program "mosquitto-clients"
 
 APP_PATH=$(cd $(dirname "$0") && pwd)
-
 echo "Application path: $APP_PATH"
 
 if [ -e $APP_PATH/requirements.txt ]
 then
     echo "Installing python dependencies ..."
-    pip3 install -r $APP_PATH/requirements.txt
+    pip3 install -U -r $APP_PATH/requirements.txt
 else
     echo "No requirements file found."
 fi
 
 
 ### add to startup ###
-if [ -e /etc/rc.local ]
-then
-    if grep -q "nohup sudo $APP_PATH/app.py &" /etc/rc.local
-    then
-        echo "Program already added on startup."
-    else
-        while true; do
-            read -p "Do you wish to add this program on startup ? " yn
-            case $yn in
-                [Yy]* ) sed -i -e "\$i \\nohup sudo $APP_PATH/app.py &\\n" /etc/rc.local; break;;
-                [Nn]* ) exit;;
-                * ) echo "Please answer yes or no.";;
-            esac
-        done
+add_to_startup "$APP_PATH/app.py"
+
+for D in cipher/plugins/*; do
+    if [ -d "${D}" ]; then
+        if [ -f ${D}/setup.sh ]; then
+            echo "Setting up plugin in ${D} ..."
+            source "${D}/setup.sh"
+            cd $APP_PATH
+        fi
     fi
-else
-    echo "No rc.local file found, can't add program on startup."
-fi
+done
